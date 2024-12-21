f9b47532847b0bfa7c04f39114ba73b881553dab
Leon Gabriel <g73346920@gmail.com>
2024-12-21 16:33:08 +0300

    chored:button set to on close when clicked

--- alx-project-0x01/components/common/PostModal.tsx
+++ alx-project-0x01/components/common/PostModal.tsx
@@ -71,6 +71,7 @@ const PostModal: React.FC<PostModalProps> = ({ onClose, onSubmit }) => {
 
             <button 
              type="submit"
+             onClick={onClose}
              className="px-6 py-2 bg-blue-500 text-white rounded-lg hover:bg-blue-600 transition">
             Add Post
             </button>
